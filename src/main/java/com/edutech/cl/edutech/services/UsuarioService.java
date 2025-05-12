81c8c3c668b6072917d23ced20050d52aabbc659
evairx <xxbrazzersxx35@gmail.com>
2025-05-12 19:58:06 -0400

    cambie totalmente el codigo de UsuariosService.java asi es mas sencillo y aprovecha la API de JPA

--- src/main/java/com/edutech/cl/edutech/services/UsuarioService.java
+++ src/main/java/com/edutech/cl/edutech/services/UsuarioService.java
@@ -1,46 +1,22 @@
 package com.edutech.cl.edutech.services;
 
 import com.edutech.cl.edutech.model.Usuario;
+import com.edutech.cl.edutech.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
-import java.util.*;
+import java.util.List;
 
 @Service
 public class UsuarioService {
     
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private UsuarioRepository usuarioRepository;
     
-    public List<Map<String, Object>> conseguir() {
-        String sql = "SELECT * FROM usuario";
-
-        List<Usuario> usuarios = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Usuario.class));
-
-        List<Map<String, Object>> usuariosFormateados = new ArrayList<>();
-        
-        for (Usuario usuario : usuarios) {
-            Map<String, Object> usuarioFormateado = new HashMap<>();
-            
-            usuarioFormateado.put("id", usuario.getId_usuario());
-            usuarioFormateado.put("apellidoPaterno", usuario.getAppaterno_usu());
-            usuarioFormateado.put("apellidoMaterno", usuario.getApmaterno_usu());
-            usuarioFormateado.put("correo", usuario.getCorreo_usu());
-            usuarioFormateado.put("fechaRegistro", usuario.getFec_registro_usu());
-            usuarioFormateado.put("nombre", usuario.getPnombre_usu());
-            usuarioFormateado.put("segundoNombre", usuario.getSnombre_usu());
-            usuarioFormateado.put("rol", usuario.getTipo_rol_usu());
-            
-            usuariosFormateados.add(usuarioFormateado);
-        }
-        
-        return usuariosFormateados;
+    public List<Usuario> conseguir() {
+        return usuarioRepository.findAll();
     }
-    
-    public List<Usuario> obtenerTodosUsuarios() {
-        String sql = "SELECT * FROM usuario";
 
-        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Usuario.class));
+    public Usuario conseguirPorId(Integer id) {
+        return usuarioRepository.encontrarPorId(id).orElse(null);
     }
 }
