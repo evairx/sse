d787d819b4073dba571153aac84bc8888f470a88
evairx <xxbrazzersxx35@gmail.com>
2025-05-12 19:56:15 -0400

    Agregue la funccion encontrarPorId en UsuarioRepository.java

--- src/main/java/com/edutech/cl/edutech/repository/UsuarioRepository.java
+++ src/main/java/com/edutech/cl/edutech/repository/UsuarioRepository.java
@@ -1,7 +1,14 @@
 package com.edutech.cl.edutech.repository;
 
-public class UsuarioRepository {
+import com.edutech.cl.edutech.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
     
-
+    @Query("SELECT u FROM Usuario u WHERE u.id_usuario = :id")
+    Optional<Usuario> encontrarPorId(Integer id);
 }
