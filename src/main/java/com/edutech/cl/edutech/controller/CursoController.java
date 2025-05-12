8e7b5403fcc74e55fd4885b1599ad189c5b34b7b
GoguX76 <se.godoyr@duocuc.cl>
2025-05-12 12:28:42 -0400

    Metodos para obtener los cursos y mostrar estos por ID

--- src/main/java/com/edutech/cl/edutech/controller/CursoController.java
+++ src/main/java/com/edutech/cl/edutech/controller/CursoController.java
@@ -1,8 +1,32 @@
 package com.edutech.cl.edutech.controller;
+import com.edutech.cl.edutech.model.Curso;
 import java.util.List;
+import java.util.ArrayList;
 
-public class CursoController {
+public class CursoController { 
 
+    private List<Curso> listaCursos =  new ArrayList<>();
 
+    public List<Curso> getCursos(){
+
+        return listaCursos;
+
+    }
+
+    public Curso showCursos(int id){
+
+        for(Curso cur : listaCursos){
+
+            if(cur.getId_curso() == id){
+
+                return cur;
+
+            }
+
+        }
+
+    return null;
+
+    }
     
 }
