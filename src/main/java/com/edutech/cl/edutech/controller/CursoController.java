1df377b82c39da411afb98645db201cb84a3456e
GoguX76 <se.godoyr@duocuc.cl>
2025-05-12 10:50:09 -0400

    Clase CursoController en Controller

---
+++ src/main/java/com/edutech/cl/edutech/controller/CursoController.java
@@ -0,0 +1,8 @@
+package com.edutech.cl.edutech.controller;
+import java.util.List;
+
+public class CursoController {
+
+
+    
+}
--- src/main/java/com/edutech/cl/edutech/services/UsuarioService.java
+++ src/main/java/com/edutech/cl/edutech/services/UsuarioService.java
@@ -2,6 +2,5 @@ package com.edutech.cl.edutech.services;
 
 public class UsuarioService {
 
-    private int olakeace;
 
 }
