3bc080f857fd308d910eb93a71cdacf1ffcfb183
SilvioPeralta <si.peralta@duocuc.cl>
2025-05-15 09:40:46 -0400

    assadwdsd

--- src/main/java/com/edutech/cl/edutech/EdutechApplication.java
+++ src/main/java/com/edutech/cl/edutech/EdutechApplication.java
@@ -2,11 +2,21 @@ package com.edutech.cl.edutech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api")
 @SpringBootApplication
 public class EdutechApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EdutechApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EdutechApplication.class, args);
+    }
+
+    @GetMapping("/usuario")
+    public String mostrarHoli() {
+        return "holi";
+    }
 }
