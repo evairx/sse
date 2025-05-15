4255fbb097a2d389a2e52206cc7d08bf464b928f
zeronight <se.godoyr@duocuc.cl>
2025-05-15 08:33:10 -0400

    Cambio en la lógica de CursoRepository. Uso innecesario de Query ya que JPARepository hace el proceso de buscar por id automaticamente

--- src/main/java/com/edutech/cl/edutech/repository/CursoRepository.java
+++ src/main/java/com/edutech/cl/edutech/repository/CursoRepository.java
@@ -1,14 +1,9 @@
 package com.edutech.cl.edutech.repository;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.edutech.cl.edutech.model.Curso;;
 
 @Repository
 public interface CursoRepository extends JpaRepository<Curso, Integer>{
-
-    @Query("SELECT c FROM Curso c WHERE c.id_curso = :id")
-    Optional<Curso> findById(Integer id);
 
 }
