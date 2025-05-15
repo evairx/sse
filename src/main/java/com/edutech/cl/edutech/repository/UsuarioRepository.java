8d8ffd3fce9f70b9258d61b4758c0903269702b0
evairx <xxbrazzersxx35@gmail.com>
2025-05-15 09:32:49 -0400

    elimine los imports no usados en Usuario Repository

--- src/main/java/com/edutech/cl/edutech/repository/UsuarioRepository.java
+++ src/main/java/com/edutech/cl/edutech/repository/UsuarioRepository.java
@@ -2,9 +2,7 @@ package com.edutech.cl.edutech.repository;
 
 import com.edutech.cl.edutech.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
