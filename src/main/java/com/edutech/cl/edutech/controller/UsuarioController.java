1c8f940a5676c3618494e64f55596d4bb6377970
SilvioPeralta <si.peralta@duocuc.cl>
2025-05-14 21:04:15 -0400

    prueba mapping

--- src/main/java/com/edutech/cl/edutech/controller/UsuarioController.java
+++ src/main/java/com/edutech/cl/edutech/controller/UsuarioController.java
@@ -1,6 +1,16 @@
 package com.edutech.cl.edutech.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api")
 public class UsuarioController {
 
+    @GetMapping("/usuario")
+    public String mostrarHoli() {
+        return new String("holi");
+    }
+    
 
 }
