046e59c7bf5a00db49d6d30d93b29ee2196ccf9e
evairx <xxbrazzersxx35@gmail.com>
2025-05-15 10:00:07 -0400

    arreglo modelo de inscripciones curso y se esta usando el controlador usuario en el main

--- src/main/java/com/edutech/cl/edutech/EdutechApplication.java
+++ src/main/java/com/edutech/cl/edutech/EdutechApplication.java
@@ -1,22 +1,16 @@
 package com.edutech.cl.edutech;
 
+import com.edutech.cl.edutech.controller.UsuarioController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.ConfigurableApplicationContext;
 
-@RestController
-@RequestMapping("/api")
 @SpringBootApplication
 public class EdutechApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EdutechApplication.class, args);
-    }
-
-    @GetMapping("/usuario")
-    public String mostrarHoli() {
-        return "holi";
+        ConfigurableApplicationContext context = SpringApplication.run(EdutechApplication.class, args);
+    
+        context.getBean(UsuarioController.class);
     }
 }
--- src/main/java/com/edutech/cl/edutech/model/InscripcionesCursos.java
+++ src/main/java/com/edutech/cl/edutech/model/InscripcionesCursos.java
@@ -2,21 +2,27 @@ package com.edutech.cl.edutech.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "inscripciones_curso")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+public class InscripcionesCursos implements Serializable {
 
-public class InscripcionesCursos {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-    @OneToMany
+    @ManyToOne
     @JoinColumn(name = "id_curso", nullable = false)
     private Curso curso;
 
     @ManyToOne
     @JoinColumn(name = "id_usuario", nullable = false)
     private Usuario usuario;
-
+    
+    @Column(name = "fecha_inscripcion")
+    private java.util.Date fechaInscripcion;
 }
