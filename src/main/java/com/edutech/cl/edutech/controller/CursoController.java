0adace7263bf7440d2c305e79a4166155b637b1c
SilvioPeralta <si.peralta@duocuc.cl>
2025-05-16 22:29:35 -0400

    curso controller mas mjr

--- src/main/java/com/edutech/cl/edutech/controller/CursoController.java
+++ src/main/java/com/edutech/cl/edutech/controller/CursoController.java
@@ -7,6 +7,12 @@ import com.edutech.cl.edutech.services.CursoService;
 import java.util.List;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
 
 
 @RestController
@@ -21,11 +27,28 @@ public class CursoController {
         return cursoService.getCursos();
     }
 
-    @PostMapping("/crear/{id}")
-    public String crear(@PathVariable("id") int id,@RequestBody String entity) {
-        //TODO: process POST request
-        
-        return entity;
+    @PostMapping("/crear")
+    public String crear(@RequestBody Curso curso) {
+        cursoService.crearCurso(curso);
+        return curso.toString();
     }
     
+    @GetMapping("/{id}")
+    public String buscarCurso(@PathVariable("id") int id,@RequestParam String param) {
+        Curso curso = cursoService.getCursoId(id);
+        return curso.toString();
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String borrarString(@PathVariable("id") int id,@RequestParam String param) {
+        Curso curso = cursoService.getCursoId(id);
+        cursoService.eliminarCurso(id);
+        return curso.toString();
+    }
+    
+    @PutMapping("/actualizar/{id}")
+    public String putMethodName(@PathVariable int id, @RequestBody Curso curso) {
+        cursoService.updateCurso(id, curso);
+        return curso.toString();
+    }
 }
