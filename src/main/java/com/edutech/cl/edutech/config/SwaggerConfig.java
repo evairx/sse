451ff2ef2eafd03c195b80aa7bf224b024e24a3e
evairx <xxbrazzersxx35@gmail.com>
2025-06-22 22:11:18 -0400

    Agregue Documentacion con Swagger y SwaggerConfig

---
+++ src/main/java/com/edutech/cl/edutech/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.edutech.cl.edutech.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Edutech API")
+                .version("1.0.0")
+                .description("API REST Oficial de edutech"));
+    }
+}
